7f3ed4a02154577d7d5f5920c8987e36db116156
shakilla1 <shakilshifat65@gmail.com>
2022-08-23 16:11:23 +0600

    add header section

---
+++ js/button.js
@@ -0,0 +1,12 @@
+
+function myMessage() {
+    alert("You cannot select more than five");
+}
+function disable() {
+    document.getElementById("btn-gray").disable = true;
+}
+// let btngray = document.querySelector('#btn-gray');
+
+// btngray.addEventListener('click', (){
+//     btngray.backgroundColor = ('bg-slate-700');
+// })
---
+++ js/player.js
@@ -0,0 +1,50 @@
+// on click button disale start 
+// let btnShow = document.querySelector('button');
+// let input = document.querySelector('input');
+// let output = document.querySelector('h5');
+// btnShow.addEventListener('click', (){
+//     output.innerText = input.value;
+// });
+// input.addEventListener.length('keyup',{
+//     if (input.value.length > 5) btnShow.disabled = true;
+//     else
+// });
+
+
+
+
+
+
+
+
+// on click button disale end 
+const cartArray = [];
+function display(cartplayer) {
+    console.log(cartplayer);
+    const tableBody = document.getElementById("cart-players");
+    tableBody.innerHTML = "";
+    for (let i = 0; i < cartplayer.length; i++) {
+        // console.log(cartArray[i].playerName);
+        const name = cartArray[i].playerName;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>${i + 1}</th>
+            <td>${name}</td> `;
+        tableBody.appendChild(tr);
+    }
+}
+function addToCart(element) {
+    // console.log(element.parentNode.parentNode.children[1])
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    const playerobj = {
+        playerName: playerName,
+    }
+    cartArray.push(playerobj);
+    // console.log(cartArray);
+    // console.log(cartArray.length);
+
+    document.getElementById("total-added player").innerText = cartArray.length;
+
+    display(cartArray);
+
+}
